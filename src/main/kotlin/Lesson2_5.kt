037408c067335d99fad1569de968e7ae48777f70
AndrewQJLIN <nut01@yandex.ru>
2023-03-26 19:09:24 +0400

    KS_2_5

--- src/main/kotlin/Lesson2_5.kt
+++ src/main/kotlin/Lesson2_5.kt
@@ -1,3 +1,13 @@
-fun main() {
+import kotlin.math.pow
+import kotlin.math.roundToInt
 
+fun main() {
+    val depositStartSum = 70_000
+    val rate = 16.7
+    val depositTime = 20
+
+    val rateFormula = (1+rate/100)
+    var depositSum= (depositStartSum * rateFormula.pow(depositTime)*1000).roundToInt()*0.001
+
+    println("Сумма депозита с %% - $depositSum")
 }
