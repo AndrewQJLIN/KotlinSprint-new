3cca23b8f8be0258d3dbb2d022fc8e1676b92316
AndrewQJLIN <nut01@yandex.ru>
2023-03-26 19:02:52 +0400

    KS_2_1

--- src/main/kotlin/Lesson2_1.kt
+++ src/main/kotlin/Lesson2_1.kt
@@ -1,3 +1,12 @@
 fun main() {
+    val stud1: Float = 3f
+    val stud2: Float = 4f
+    val stud3: Float = 3f
+    val stud4: Float = 5f
 
+    val totalStuds = 4
+
+    val averageMark = (stud1+stud2+stud3+stud4)/totalStuds
+
+    println("Average mark of class - $averageMark")
 }
