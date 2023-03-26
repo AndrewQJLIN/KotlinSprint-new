72a0445d62a618026fb956cc74ba96b65c59613a
AndrewQJLIN <nut01@yandex.ru>
2023-03-26 19:05:23 +0400

    KS_2_2

--- src/main/kotlin/Lesson2_2.kt
+++ src/main/kotlin/Lesson2_2.kt
@@ -1,3 +1,15 @@
 fun main() {
+    val oldWorkers = 50
+    val salaryOldWorkers = 30000
 
+    val newStagers = 30
+    val salaryStagers = 20000
+
+    val oldStaffSalary = oldWorkers*salaryOldWorkers
+    val totalSalary = oldStaffSalary + newStagers*salaryStagers
+    val averageSalary = totalSalary/(oldWorkers+newStagers)
+
+    println("Расходы на выплату зарплаты постоянных сотрудников: $oldStaffSalary рублей")
+    println("Общие расходы по ЗП после прихода стажеров: $totalSalary рублей")
+    println("Среднюю ЗП одного сотрудника после устройства стажеров: $averageSalary рублей")
 }
