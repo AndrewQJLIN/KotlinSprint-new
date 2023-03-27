28a57e87d74ca0b035d5b3087243a9a697ce7597
AndrewQJLIN <nut01@yandex.ru>
2023-03-27 18:37:06 +0400

    KS_3_3 калькулятор принт

---
+++ src/main/kotlin/lessons3/Lesson3_3.kt
@@ -0,0 +1,20 @@
+package lessons3
+
+fun main() {
+    val digitCalculated = 4
+
+    println(
+        """
+            1 x $digitCalculated = ${1*digitCalculated}
+            2 x $digitCalculated = ${2*digitCalculated}
+            3 x $digitCalculated = ${3*digitCalculated}
+            4 x $digitCalculated = ${4*digitCalculated}
+            5 x $digitCalculated = ${5*digitCalculated}
+            6 x $digitCalculated = ${6*digitCalculated}
+            7 x $digitCalculated = ${7*digitCalculated}
+            8 x $digitCalculated = ${8*digitCalculated}
+            9 x $digitCalculated = ${9*digitCalculated}
+        """.trimIndent()
+    )
+
+}
