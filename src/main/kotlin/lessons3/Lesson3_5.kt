e9b535545f8abb4418c51343b5f1ddd62978ac4c
AndrewQJLIN <nut01@yandex.ru>
2023-03-28 22:07:59 +0400

    KS_3_5 - новый парсинг

--- src/main/kotlin/lessons3/Lesson3_5.kt
+++ src/main/kotlin/lessons3/Lesson3_5.kt
@@ -3,14 +3,12 @@ package lessons3
 fun main() {
     val playerMove = "D2-D4;0"
 
-    val startMoveLetter = "${playerMove[0]}"
-    val startMoveDigit = "${playerMove[1]}"
-    val endMoveLetter = "${playerMove[3]}"
-    val endMoveDigit = "${playerMove[4]}"
-    val countMove = "${playerMove[6]}"
+    val startMove = playerMove.substringBefore('-')
+    val endMove = playerMove.substringAfter('-').substringBefore(';')
+    val countMove = playerMove.substringAfter(';')
 
     println("Ход игрока номер: $countMove")
-    println("Фигура перемещается с клетки: $startMoveLetter$startMoveDigit")
-    println("на клетку: $endMoveLetter$endMoveDigit")
+    println("Фигура перемещается с клетки: $startMove")
+    println("на клетку: $endMove")
 
 }
