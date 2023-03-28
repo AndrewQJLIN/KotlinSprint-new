3deb3bf3fac94b064d1a7943b8a829a6db126440
AndrewQJLIN <nut01@yandex.ru>
2023-03-28 21:54:09 +0400

    KS_3_4 = только 3 переменные

--- src/main/kotlin/lessons3/Lesson3_4.kt
+++ src/main/kotlin/lessons3/Lesson3_4.kt
@@ -3,24 +3,17 @@ package lessons3
 fun main() {
     var strokeNumber = 1
 
-    var startPosLetter = "E"
-    var startPosDigit = 2
+    var startPos = "E2"
 
-    var endPosLetter = "E"
-    var endPosDigit = 4
+    var endPos = "E4"
 
-    var playersMove = "[$startPosLetter$startPosDigit-$endPosLetter$endPosDigit;$strokeNumber]"
+    var playersMove = "[$startPos-$endPos;$strokeNumber]"
     println(playersMove)
 
     strokeNumber++
+    startPos = "D2"
+    endPos = "D3"
 
-    startPosLetter = "D"
-    startPosDigit = 2
-
-    endPosLetter = startPosLetter
-    endPosDigit = startPosDigit + 1
-
-    playersMove = "[$startPosLetter$startPosDigit-$endPosLetter$endPosDigit;$strokeNumber]"
+    playersMove = "[$startPos-$endPos;$strokeNumber]"
     println(playersMove)
-
 }
