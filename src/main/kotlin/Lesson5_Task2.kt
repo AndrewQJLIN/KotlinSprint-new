660fe121c7c914bb2751ef27886cf83a30eafdba
AndrewQJLIN <nut01@yandex.ru>
2023-03-29 21:29:31 +0400

    KS_5_2 вход по году рождения

---
+++ src/main/kotlin/Lesson5_Task2.kt
@@ -0,0 +1,14 @@
+import java.time.LocalDate
+
+fun main() {
+
+    println("Введите год Вашего рождения:")
+    val usersYear = readln().toInt()
+    val currentYear = LocalDate.now().year
+
+    val resultText = if ((currentYear - usersYear) > AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
+    else "Доступ запрещен - нет полных 18 лет"
+    println(resultText)
+}
+
+const val AGE_OF_MAJORITY = 18
