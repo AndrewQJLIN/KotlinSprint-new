b1f075bd8cd73d4c98c8e6e1bbfa95424e1f330e
AndrewQJLIN <nut01@yandex.ru>
2023-03-29 17:46:34 +0400

    KS_4_2 Average - исправил VAL - формат кода

--- src/main/kotlin/lesson4/Lesson4_Task1.kt
+++ src/main/kotlin/lesson4/Lesson4_Task1.kt
@@ -2,10 +2,10 @@ package lesson4
 
 fun main() {
 
-    var bookingToday = 13
-    var bookingTomorrow = 4
+    val bookingToday = 13
+    val bookingTomorrow = 4
 
-    println("Доступность столиков на сегодня:${bookingToday< TOTAL_TABLES} \nДоступность столиков на завтра:${bookingTomorrow< TOTAL_TABLES}")
+    println("Доступность столиков на сегодня:${bookingToday < TOTAL_TABLES} \nДоступность столиков на завтра:${bookingTomorrow < TOTAL_TABLES}")
 }
 
 const val TOTAL_TABLES = 13
