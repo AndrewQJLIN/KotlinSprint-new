7ae6c9c154587cf81ee18900a4b3353bb05a6c31
AndrewQJLIN <nut01@yandex.ru>
2023-03-30 12:40:45 +0400

    KS_4_5 исправил на readln()

--- src/main/kotlin/Lesson4_Task5.kt
+++ src/main/kotlin/Lesson4_Task5.kt
@@ -2,21 +2,14 @@ import java.util.Scanner
 
 fun main() {
 
-//    var conditionStatus = true
-//    var crewSize = 0
-//    var provisionBoxes = 0
-//    var weatherStatusIsGood = true
-
-    val read = Scanner(System.`in`)
-
     println("Есть ли провреждения корпуса (напишите)?\nfalse - если НЕТ повреждений\ntrue - если ДА, ЕСТЬ повреждения")
-    val conditionStatus = read.nextBoolean()
+    val conditionStatus = readln().toBoolean()
     println("Введите численность экипажа (число):")
-    val crewSize = read.nextInt()
+    val crewSize = readln().toInt()
     println("Введите число ящиков провизии (число):")
-    val provisionBoxes = read.nextInt()
+    val provisionBoxes = readln().toInt()
     println("Благоприятна ли погода (напишите)?\nfalse - если НЕТ, погода НЕ благоприятная\ntrue - если ДА, погода благоприятная")
-    val weatherStatusIsGood = read.nextBoolean()
+    val weatherStatusIsGood = readln().toBoolean()
 
     val readyToStart =
         ((!conditionStatus && (crewSize >= MINIMUM_CREW_SIZE && crewSize < MAXIMUM_CREW_SIZE)) ||
