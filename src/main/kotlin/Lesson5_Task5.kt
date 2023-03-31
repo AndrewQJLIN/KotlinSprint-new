4371c3444bb56acadd05b97b9839daae52a32060
AndrewQJLIN <nut01@yandex.ru>
2023-03-31 20:57:22 +0400

    KS_5_5 - правки

--- src/main/kotlin/Lesson5_Task5.kt
+++ src/main/kotlin/Lesson5_Task5.kt
@@ -1,5 +1,5 @@
 fun main() {
-    var gameNumbers = intArrayOf(0, 0)
+    val gameNumbers = intArrayOf(0, 0)
 
     println(
         "Угадай два числа в диапазоне от ${START_NUMBER} до ${END_NUMBER} включительно\n" +
@@ -11,6 +11,7 @@ fun main() {
             gameNumbers[0] = (START_NUMBER..END_NUMBER).random()
             gameNumbers[1] = (START_NUMBER..END_NUMBER).random()
         } while (gameNumbers[0] == gameNumbers[1])
+        val strAnswer = String.format("%s %d %d", STR_PAIR_NUMBERS_, gameNumbers[0], gameNumbers[1])
 
         var result = 0
 
@@ -24,24 +25,24 @@ fun main() {
 
         val outputText = when (result) {
             1 -> {
-                firstUserNumber = 0; "Вы выиграли утешительный приз!"
+                firstUserNumber = 0
+                "Вы выиграли утешительный приз!"
             }
 
             2 -> {
-                firstUserNumber = 0; "Поздравляем! Вы выиграли главный приз!"
+                firstUserNumber = 0
+                "Поздравляем! Вы выиграли главный приз!"
             }
 
-            else -> {
-                println("НЕУГАДАЛИ! Были загаданы числа:" + gameNumbers[0] + " " + gameNumbers[1])
-                "Неудача! Крутите барабан!"
-            }
+            else -> "Неудача! Крутите барабан!"
         }
-        println(outputText)
+        println(outputText + strAnswer)
 
     } while (firstUserNumber != 0)
-    println("Игра окончена\nБыли загаданы числа:" + gameNumbers[0] + " " + gameNumbers[1])
+    println("Игра окончена!")
 
 }
 
 const val START_NUMBER = 1
 const val END_NUMBER = 100
+const val STR_PAIR_NUMBERS_ = "Были загаданы числа: "
