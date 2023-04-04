79ea00620fd28ab47733d030d9bdc286442e5de5
AndrewQJLIN <nut01@yandex.ru>
2023-04-04 22:35:04 +0400

    KS_8_3

--- src/main/kotlin/lesson8/Lesson8_3.kt
+++ src/main/kotlin/lesson8/Lesson8_3.kt
@@ -1,4 +1,9 @@
 package lesson8
 
-class Lesson8_3 {
+fun main() {
+    val receptOne = arrayOf("рис", "лук", "морковь", "масло", "соль", "мясо")
+    println("Введите искомый ингредиент")
+
+    if (receptOne.contains(readln())) println("Этот ингредиент в рецепте есть")
+    else println("Такого ингредиента в рецепте нет")
 }
