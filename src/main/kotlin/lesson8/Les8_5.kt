9b9c7a956f5f7ab931b8af04ce2cf0f76bfe14e6
AndrewQJLIN <nut01@yandex.ru>
2023-04-05 15:08:13 +0400

    KS_8_5

---
+++ src/main/kotlin/lesson8/Les8_5.kt
@@ -0,0 +1,12 @@
+package lesson8
+
+fun main() {
+    val receptList = mutableListOf<String>()
+    println("Введите число ингредиентов рецепта")
+    val countIngredients = readln().toInt()
+
+    for (i in 0 until countIngredients) {
+        println("Введите ингредиент №${i + 1}")
+        receptList.add(readln())
+    }
+    println("Ваш список ингредиентов")
