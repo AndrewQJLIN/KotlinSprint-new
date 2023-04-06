106c7533def3f9db32e4b78da27357d614885302
AndrewQJLIN <nut01@yandex.ru>
2023-04-06 12:23:13 +0400

    KS_9_3 второй вариант

--- src/main/kotlin/Lesson9_Task3.kt
+++ src/main/kotlin/Lesson9_Task3.kt
@@ -4,15 +4,13 @@ fun main() {
     val omletMeasure = listOf<String>("шт.", "мл.", "гр")
     val omletCount = listOf<Int>(2, 50, 15)
 
+
     println("Какое количество порций омлета надо приготовить? Введи число:")
     val count = readln().toInt()
 
-    println(
-        "На $count порций Вам понадобится:\n" +
-                "${omletIngredient[0]} - ${omletCount[0] * count} ${omletMeasure[0]}\n" +
-                "${omletIngredient[1]} - ${omletCount[1] * count} ${omletMeasure[1]}\n" +
-                "${omletIngredient[2]} - ${omletCount[2] * count} ${omletMeasure[2]}\n"
-    )
-
+    println("На $count порций Вам понадобится:")
+    for(i in omletIngredient.indices) {
+        println("${omletIngredient[i]} - ${omletCount[i] * count} ${omletMeasure[i]}")
+    }
 
 }
