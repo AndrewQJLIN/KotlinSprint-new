64595ddc3e50876ef13b4e1d72dd0238b0ea0b2d
AndrewQJLIN <nut01@yandex.ru>
2023-04-06 12:25:17 +0400

    KS_9_4

--- src/main/kotlin/Lesson9_Task4.kt
+++ src/main/kotlin/Lesson9_Task4.kt
@@ -1,6 +1,6 @@
 fun main() {
     println("Введите пять ингредиетов через заяпятую в одну строку")
-    val listIngredient: List<String> = (readlnOrNull()!!.split(',')).sorted()
+    val listIngredient: List<String> = readln().split(',').sorted()
     println(listIngredient)
 
 }
