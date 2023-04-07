a321c0e6c58c87410bb0475a754cb382dde949af
AndrewQJLIN <nut01@yandex.ru>
2023-04-07 22:40:27 +0400

    KS_10_3

---
+++ src/main/kotlin/Lesson10_3.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    println("Введите длину пароля")
+    val passOut = getPass(readln().toInt())
+
+    println("Ваш пароль: $passOut")
+}
+
+fun getPass(passLen: Int): String {
+    val rangeDigit = 0..9
+    val rangeSymbolOne = 33..47
+    val rangeSymbolTwo = 58..64
+    val rangeSymbolThree = 91..96
+    val rangeSymbolFour = 123..126
+    val rang = rangeSymbolOne + rangeSymbolTwo + rangeSymbolThree + rangeSymbolFour
+
+    var passOut=""
+
+    for (i in 1..passLen) {
+        passOut += if (i % 2 == 0)
+            rangeDigit.random().toString()
+        else
+            rang.random().toChar().toString()
+    }
+    return passOut
+}
+
