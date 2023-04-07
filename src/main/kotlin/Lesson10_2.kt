ee6a55631787fc220ecf805be0f845adf8c1c8ea
AndrewQJLIN <nut01@yandex.ru>
2023-04-07 22:09:18 +0400

    KS_10_2

---
+++ src/main/kotlin/Lesson10_2.kt
@@ -0,0 +1,14 @@
+
+
+fun main() {
+    println("Enter your LOGIN")
+    val userLogin = readln()
+    println("Enter your PASSWORD")
+    val userPassword = readln()
+
+    if(checkString(userLogin) || checkString(userPassword))
+        println("Логин или пароль недостаточно длинные")
+
+}
+
+fun checkString(strName: String): Boolean = (strName.length < 4)
