47666e01d63a118f4ae477418481274de47b81e5
AndrewQJLIN <nut01@yandex.ru>
2023-04-08 15:38:49 +0400

    KS_11_1 two users and print

---
+++ src/main/kotlin/Lesson11_Task1/Main.kt
@@ -0,0 +1,22 @@
+package Lesson11_Task1
+
+fun main() {
+    val userOne = User(
+        id = 1,
+        login = "Andrew",
+        password = "qwerty",
+        eMail = "andrew_nut@ru.ru"
+    )
+    val userTwo = User(
+        id = 2,
+        login = "Nike",
+        password = "123456",
+        eMail = "loopy@mail.ru"
+    )
+    println(printData(userOne))
+    println(printData(userTwo))
+}
+
+fun printData(user:User):String {
+    return "ID = ${user.id}, LOGIN = ${user.login}, PASSWORD = ${user.password}, EMAIL = ${user.eMail}"
+}
---
+++ src/main/kotlin/Lesson11_Task1/User.kt
@@ -0,0 +1,8 @@
+package Lesson11_Task1
+
+class User (
+    val id:Int,
+    val login:String,
+    val password:String,
+    val eMail:String,
+)
