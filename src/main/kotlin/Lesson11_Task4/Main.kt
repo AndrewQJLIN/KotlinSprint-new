e45a5614d38ef07fb35769c61d295bb670767855
AndrewQJLIN <nut01@yandex.ru>
2023-04-08 17:47:54 +0400

    KS_11_4

---
+++ src/main/kotlin/Lesson11_Task4/Main.kt
@@ -0,0 +1,11 @@
+import Lesson11_Task4.Contact
+
+fun main() {
+
+val screenShotContact = Contact(
+    phoneNumCellular = "8 (999) 999 - 42 - 42",
+    phoneNumHome = "8 (999) 888 - 42 - 42",
+    listNearContact = mutableListOf(Contact("жена"), Contact("подруга"), Contact("подруга"))
+)
+
+}
