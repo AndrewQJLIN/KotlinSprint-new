62cda0e1e0763d12bd61b98acefef8efcb5b731e
AndrewQJLIN <nut01@yandex.ru>
2023-04-09 14:05:54 +0400

    KS_12_5

---
+++ src/main/kotlin/Lesson12_Task5/DayWeatherCalc.kt
@@ -0,0 +1,28 @@
+package Lesson12_Task5
+
+import java.time.LocalDate
+
+class DayWeatherCalc(
+    val dayName: LocalDate,
+    var tempDay: Int,
+    var tempNight: Int,
+    var rainIsIn: Int = 0,
+    var atmosPressure: Int,
+) {
+
+    init {
+        this.printData()
+    }
+
+    private fun printData() {
+        println(
+            """Состояние погоды на ${dayName}:
+            |Температура днем: ${tempDay}°С
+            |Температура днем: ${tempNight}°С
+            |Дождь был: ${if (rainIsIn == 0) "Нет" else "Да"}
+            |Атосферное давление: ${atmosPressure} мм.рт.ст
+            |
+        """.trimMargin()
+        )
+    }
+}
