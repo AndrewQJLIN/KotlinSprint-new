592c1ea9bc0591265376d327eee9e196c7326e1e
AndrewQJLIN <nut01@yandex.ru>
2023-04-10 21:01:30 +0400

    KS_12_5 final

--- src/main/kotlin/Lesson12_Task5/DayWeatherCalc.kt
+++ src/main/kotlin/Lesson12_Task5/DayWeatherCalc.kt
@@ -2,6 +2,43 @@ package Lesson12_Task5
 
 import java.time.LocalDate
 
+fun main() {
+    val daysWeather = mutableListOf<DayWeatherCalc>()
+
+    var averageDayTemp = 0
+    var averageNightTemp = 0
+    var averageAtmosPressure = 0
+    var countRainDays = 0
+
+    for (i in 1..10) {
+        daysWeather += DayWeatherCalc(
+            dayName = LocalDate.of(2023, 4, (1..30).random()),
+            tempDay = (0..30).random(),
+            tempNight = (-5..20).random(),
+            atmosPressure = (730..770).random(),
+            rainIsIn = (0..1).random()
+        )
+    }
+
+    daysWeather.forEach {
+        countRainDays += it.rainIsIn
+        averageDayTemp += it.tempDay
+        averageNightTemp += it.tempNight
+        averageAtmosPressure += it.atmosPressure
+    }
+    val countDays = daysWeather.size
+
+    println(
+        """Средние значения за период :
+            |Температура днем: ${averageDayTemp / countDays}°С
+            |Температура ночью: ${averageNightTemp / countDays}°С
+            |Атосферное давление: ${averageAtmosPressure / countDays} мм.рт.ст
+            |Количество дождливых дней: $countRainDays"
+            |
+        """.trimMargin()
+    )
+}
+
 class DayWeatherCalc(
     var dayName: LocalDate,
     var tempDay: Int,
