fd68ac29b14a438bda6306e6dbe80481e7715191
AndrewQJLIN <nut01@yandex.ru>
2023-04-10 21:32:28 +0400

    KS_13_5 final

--- src/main/kotlin/Lesson13_5/Main.kt
+++ src/main/kotlin/Lesson13_5/Main.kt
@@ -1,16 +1,12 @@
 package Lesson13_5
 
 fun main() {
-    val number: Long
-    val strNumber = readLine().toString()
+    val strNumber = readln()
     try {
         println("Ваши цифры - ${strNumber.toLong()}")
-    }
-    catch (e: NumberFormatException) {
-          println("В номере должны быть только цифры, а не это $strNumber")
-    }
-    finally {
+    } catch (e: NumberFormatException) {
+        println("В номере должны быть только цифры, а не это $strNumber")
+    } finally {
         println("Мы закончили")
     }
-
 }
