aaf53f4ea7df8e8cd50105c401341b8d43a6585b
AndrewQJLIN <nut01@yandex.ru>
2023-04-10 20:40:47 +0400

    KS_12_1 final

--- src/main/kotlin/lesson12_Task1/WeatherStatus.kt
+++ src/main/kotlin/lesson12_Task1/WeatherStatus.kt
@@ -2,6 +2,23 @@ package lesson12_Task1
 
 import java.time.LocalDate
 
+fun main() {
+    val day1 = WeatherStatus()
+    val day2 = WeatherStatus()
+
+    day1.printData()
+    day2.printData()
+
+    day1.atmosPressure = 739
+    day2.atmosPressure = 790
+    day1.tempDay = 23
+    day2.tempNight = -5
+    day1.rainIsIn = if (day1.rainIsIn == 0) 1 else 0
+    day2.rainIsIn = if (day2.rainIsIn == 0) 1 else 0
+    day1.printData()
+    day2.printData()
+}
+
 class WeatherStatus() {
     val dayName = LocalDate.of(2023, 4, (1..30).random())
     var tempDay = (0..30).random()
@@ -9,13 +26,15 @@ class WeatherStatus() {
     var rainIsIn = (0..1).random()
     var atmosPressure = (730..770).random()
 
-    fun printData () {
-        println("""Состояние погоды на ${dayName}:
+    fun printData() {
+        println(
+            """Состояние погоды на ${dayName}:
             |Температура днем: ${tempDay}°С
             |Температура днем: ${tempNight}°С
-            |Дождь был: ${if(rainIsIn == 0) "Нет" else "Да" }
+            |Дождь был: ${if (rainIsIn == 0) "Нет" else "Да"}
             |Атосферное давление: ${atmosPressure} мм.рт.ст 
             |
-        """.trimMargin())
+        """.trimMargin()
+        )
     }
 }
