3a093b22f347af61f03c23a54f67d864fd2c1cf4
AndrewQJLIN <nut01@yandex.ru>
2023-04-10 20:23:06 +0400

    KS_11_2 в один файл

--- src/main/kotlin/Lesson11_Task2/Main.kt
+++ src/main/kotlin/Lesson11_Task2/Main.kt
@@ -17,3 +17,38 @@ fun main() {
     userOne.sendMessage(readln())
 
 }
+
+class User(
+    val id: Int,
+    val login: String,
+    var password: String,
+    val eMail: String,
+    var bio: String = ""
+) {
+    fun printUserData() {
+        println("ID = $id, LOGIN = $login, PASSWORD = $password, EMAIL = $eMail, BIO = $bio")
+    }
+
+    fun setBio() {
+        println("Введите информацию о себе - раздел BIO")
+        bio = readln()
+    }
+
+    fun changePassword() {
+        do {
+            println("Введите старый пароль")
+            if (password == readln()) {
+                println("Введите новый пароль")
+                password = readln()
+                println("Пароль изменен")
+                break
+            } else {
+                println("Не верный пароль")
+            }
+        } while (true)
+    }
+
+    fun sendMessage(message: String) {
+        println("Сообщение $message\nотправлено на почту $eMail адресату $login")
+    }
+}
