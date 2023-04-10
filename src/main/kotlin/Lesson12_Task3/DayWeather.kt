2da2f4043a4a46d7c606387c83050f1252c0a659
AndrewQJLIN <nut01@yandex.ru>
2023-04-10 20:54:23 +0400

    KS_12_3 final

--- src/main/kotlin/Lesson12_Task3/DayWeather.kt
+++ src/main/kotlin/Lesson12_Task3/DayWeather.kt
@@ -2,21 +2,37 @@ package Lesson12_Task3
 
 import java.time.LocalDate
 
-class DayWeather (
-    val dayName:LocalDate,
-    var tempDay:Int,
-    var tempNight:Int,
-    var rainIsIn:Int = 0,
-    var atmosPressure:Int,
-)
-{
-    fun printData () {
-        println("""Состояние погоды на ${dayName}:
+
+fun main() {
+    val daysWeather = mutableListOf<DayWeather>()
+
+    for (i in 0..5) {
+        daysWeather += DayWeather(
+            dayName = LocalDate.of(2023, 4, (1..30).random()),
+            tempDay = (0..30).random(),
+            tempNight = (-5..20).random(),
+            atmosPressure = (730..770).random()
+        )
+    }
+    daysWeather.forEach { it.printData() }
+}
+
+class DayWeather(
+    val dayName: LocalDate,
+    var tempDay: Int,
+    var tempNight: Int,
+    var rainIsIn: Int = 0,
+    var atmosPressure: Int,
+) {
+    fun printData() {
+        println(
+            """Состояние погоды на ${dayName}:
             |Температура днем: ${tempDay}°С
             |Температура днем: ${tempNight}°С
-            |Дождь был: ${if(rainIsIn == 0) "Нет" else "Да"}
+            |Дождь был: ${if (rainIsIn == 0) "Нет" else "Да"}
             |Атосферное давление: ${atmosPressure} мм.рт.ст
             |
-        """.trimMargin())
+        """.trimMargin()
+        )
     }
 }
