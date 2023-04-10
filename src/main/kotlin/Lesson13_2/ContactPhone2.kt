b87660055615c65672e78ef27e5aed64ed776bfb
AndrewQJLIN <nut01@yandex.ru>
2023-04-10 21:09:55 +0400

    KS_13_2 final

--- src/main/kotlin/Lesson13_2/ContactPhone2.kt
+++ src/main/kotlin/Lesson13_2/ContactPhone2.kt
@@ -1,13 +1,21 @@
 package Lesson13_2
 
+fun main() {
+    val cont1 = ContactPhone2(89123456789, "Ростислав", "Reddit")
+    cont1.printContact()
+
+    cont1.companyName = null
+
+    cont1.printContact()
+}
+
 class ContactPhone2(
     private val phoneNumber: Long,
     private val phoneName: String,
     var companyName: String?,
 
-    )
-{
-fun printContact() {
-    println("Имя: $phoneName\nНомер: $phoneNumber\nКомпания: ${companyName ?: "[не указано]"} \n")
-}
+    ) {
+    fun printContact() {
+        println("Имя: $phoneName\nНомер: $phoneNumber\nКомпания: ${companyName ?: "[не указано]"} \n")
+    }
 }
