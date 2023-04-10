065fcfbfdf1cc18f1ebcdb928c4a6d1f14d91994
AndrewQJLIN <nut01@yandex.ru>
2023-04-10 23:34:05 +0400

    KS_14_2

---
+++ src/main/kotlin/lesson14_2/Lesson14_Task2.kt
@@ -0,0 +1,87 @@
+package lesson14_2
+
+
+fun main() {
+
+    val ship1 = IcebreakerShip("Неустрашимый", 50, 300, 1998, 2.4f, 180)
+    val ship2 = CargoShip("Игорь Светлов", 100, 3000, 2003, 150, 5000)
+
+    ship1.shipDescription()
+    println()
+    ship2.shipDescription()
+    println()
+    ship1.shipSpeed()
+    ship2.shipSpeed()
+    println()
+    ship1.startScoutDrone()
+    ship2.goToPort("СОЧИ")
+
+}
+
+open class BaseShip(
+    val shipName: String,
+    private val shipSpeed: Int,
+    private val shipLoadCapacity: Int,
+    private val dateIssue: Int,
+) {
+    fun shipSpeed() {
+        println("\"$shipName\" движеться со скоростью $shipSpeed узлов")
+    }
+
+
+    open fun shipDescription() {
+        println(
+            """
+            Название судна: "$shipName"
+            Максимальная скорость: $shipSpeed узлов
+            Объем загрузки контейнеров: $shipLoadCapacity
+            Год выпуска: $dateIssue
+        """.trimIndent()
+        )
+    }
+}
+
+class IcebreakerShip(
+    shipName: String, shipSpeed: Int, shipLoadCapacity: Int, dateIssue: Int,
+    val maxIceSize: Float,
+    val maxDaysNavigation: Int,
+) : BaseShip(shipName, shipSpeed, shipLoadCapacity, dateIssue) {
+    fun startScoutDrone() {
+        println("Ледокол $shipName запустил дроны-разведчики")
+    }
+
+
+    override fun shipDescription() {
+        super.shipDescription()
+        println(
+            """
+            Тип судна: ЛЕДОКОЛ
+            MAX толщина льда: $maxIceSize метр.
+            MAX дней навигации: $maxDaysNavigation
+        """.trimIndent()
+        )
+    }
+}
+
+class CargoShip(
+    shipName: String, shipSpeed: Int, shipLoadCapacity: Int, dateIssue: Int,
+    val maxCountConteiners: Int,
+    val maxDistance: Int,
+) : BaseShip(shipName, shipSpeed, shipLoadCapacity, dateIssue) {
+
+
+    fun goToPort(port: String) {
+        println("Грузовой корабль $shipName взял курс на порт $port")
+    }
+
+    override fun shipDescription() {
+        super.shipDescription()
+        println(
+            """
+            Тип судна: ГРУЗОВОЙ
+            MAX число контейнеров: $maxCountConteiners шт.
+            MAX длина пути: $maxDistance миль
+        """.trimIndent()
+        )
+    }
+}
