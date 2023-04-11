0f87b1ca81856d7e6d71d916b2ff5c3df28e7b2c
AndrewQJLIN <nut01@yandex.ru>
2023-04-11 20:31:45 +0400

    KS_11_5

--- src/main/kotlin/Lesson11_Task5/Main.kt
+++ src/main/kotlin/Lesson11_Task5/Main.kt
@@ -4,14 +4,32 @@ fun main() {
 
     val forumNew = Forum()
 
-    forumNew.newUser()
-    forumNew.newUser()
+    do {
+        println("Введите Ваш никнэйм")
+        val nickName = readln()
+    } while (forumNew.newUser(nickName))
+
+    do {
+        println("Введите Ваш никнэйм")
+        val nickName = readln()
+    } while (forumNew.newUser(nickName))
 
 
-    forumNew.newMessage(0, "Привет мир")
-    forumNew.newMessage(1, "привет всем")
-    forumNew.newMessage(0, "Я туту")
-    forumNew.newMessage(1, "Огого")
+    do {
+        println("Для ввода сообщения введите Ваш Id")
+        val idItem = readln().toInt()
+        println("Введите Ваше сообщение")
+        val messageItem = readln()
+        if (forumNew.newMessage(idItem, messageItem)) {
+            println("Сообщение добавлено на форум")
+        } else {
+            println("Неверные данные! Сообщение не добавлено")
+        }
+        println("Хотите продолжить = да / нет")
+
+        if (readln() != "да") break
+
+    } while (true)
 
     forumNew.printThread()
 }
@@ -19,39 +37,39 @@ fun main() {
 class MessageForum(
     val authorId: Int,
     val message: String,
-) {
+)
 
-}
+class User(
+    val userName: String,
+    val userId: Int
+)
 
 class Forum(
-    private val forumUsers: MutableList<String> = mutableListOf(),
+    private val forumUsers: MutableSet<User> = mutableSetOf<User>(),
     private val messageForum: MutableList<MessageForum> = mutableListOf(),
+) {
 
-    ) {
 
-    fun newUser() {
-        do {
-            println("Введите Ваш никнэйм")
-            val nickName = readln()
+    fun newUser(nickName: String): Boolean {
+        forumUsers.forEach { if (it.userName == nickName) return true }
 
-            if (!forumUsers.contains(nickName)) {
-                forumUsers += nickName
-                println("Привет $nickName - Вы добавлены на форум - Ваш ID ${forumUsers.lastIndex}")
-                break
-            } else {
-                println("Такой никнэйм уже есть - введите новый")
-            }
-        } while (true)
+        forumUsers += User(nickName, forumUsers.size + 1)
+        println("Привет ${forumUsers.last().userName} - Вы добавлены на форум - Ваш ID ${forumUsers.last().userId}")
+        return false
     }
 
-    fun newMessage(authorId: Int, message: String) {
+    fun newMessage(authorId: Int, message: String): Boolean {
+        var i: Int = 0
+        forumUsers.forEach { if (it.userId == authorId) i++ }
+        if (i == 0) return false
         messageForum.add(MessageForum(authorId, message))
+        return true
     }
 
     fun printThread() {
-        messageForum.forEach {
-            println("${forumUsers[it.authorId]} : ${it.message}")
+        messageForum.forEach { message ->
+            print("${forumUsers.filter { it.userId == message.authorId }[0].userName}: ")
+            println(message.message)
         }
-
     }
 }
