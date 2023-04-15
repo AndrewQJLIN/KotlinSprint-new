769bc5f454d0065344829489417ae650e6a7ce30
AndrewQJLIN <nut01@yandex.ru>
2023-04-15 19:00:59 +0400

    KS_17_2 корабль

---
+++ src/main/kotlin/Lesson17_Task2/Lesson17_2.kt
@@ -0,0 +1,27 @@
+package Lesson17_Task2
+
+fun main() {
+val newShip = BattleShip("Неустрашимый",25,"Мурманск")
+    newShip.printShipData()
+    println("Введите новое имя корабля")
+    newShip.name = readln()
+    println()
+    newShip.printShipData()
+}
+
+class BattleShip(name:String, private val averageSpeed:Int, private val portHome:String )
+{
+   var name:String = name
+        set(value) {println("Имя корабля нельзя изменить")}
+
+
+    fun printShipData()
+    {
+        println("""
+            ИМЯ: $name
+            СРЕДНЯЯ СКРОСТЬ: $averageSpeed
+            ПОРТ ПРИПИСКИ: $portHome
+        """.trimIndent())
+    }
+
+}
