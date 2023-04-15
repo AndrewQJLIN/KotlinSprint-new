243f9fb595c1f780b122c692cc4a9452f11792f8
AndrewQJLIN <nut01@yandex.ru>
2023-04-15 14:39:59 +0400

    KS_16_1 кубик

---
+++ src/main/kotlin/Lesson16_Task1/Lesson16_1.kt
@@ -0,0 +1,14 @@
+package Lesson16_Task1
+
+fun main() {
+    val diceRoll1 = DiceRoll()
+    diceRoll1.getSideDiceRoll()
+
+}
+
+class DiceRoll() {
+    private var sideDiceRoll: Int = (1..6).random()
+    fun getSideDiceRoll() {
+        println("На кубике выпало $sideDiceRoll")
+    }
+}
