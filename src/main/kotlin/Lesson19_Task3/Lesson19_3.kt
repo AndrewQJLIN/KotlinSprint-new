f1b9af7fe8e7430c03f2902b7a3cedf9245d80b0
AndrewQJLIN <nut01@yandex.ru>
2023-04-16 17:06:00 +0400

    KS_19_3 c ошибкой

---
+++ src/main/kotlin/Lesson19_Task3/Lesson19_3.kt
@@ -0,0 +1,20 @@
+package Lesson19_Task3
+
+fun main() {
+    val newShip = SpaceShip()
+    newShip.takeOff()
+}
+
+class SpaceShip {
+    fun takeOff() {
+        TODO()
+    }
+
+    fun landing() {
+        TODO()
+    }
+
+    fun shootingAsteroids() {
+
+    }
+}
