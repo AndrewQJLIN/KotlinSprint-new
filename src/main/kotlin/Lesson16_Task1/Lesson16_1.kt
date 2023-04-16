ed5e90c66e3216264c42281885fa9a9e40044795
AndrewQJLIN <nut01@yandex.ru>
2023-04-16 12:48:41 +0400

    KS_16_1

--- src/main/kotlin/Lesson16_Task1/Lesson16_1.kt
+++ src/main/kotlin/Lesson16_Task1/Lesson16_1.kt
@@ -3,11 +3,10 @@ package Lesson16_Task1
 fun main() {
     val diceRoll1 = DiceRoll()
     diceRoll1.getSideDiceRoll()
-
 }
 
-class DiceRoll() {
-    private var sideDiceRoll: Int = (1..6).random()
+class DiceRoll {
+    private val sideDiceRoll: Int = (1..6).random()
     fun getSideDiceRoll() {
         println("На кубике выпало $sideDiceRoll")
     }
