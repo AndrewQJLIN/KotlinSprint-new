04c30d4e2d5d94bdcda942db08be5f1c1fd4438c
AndrewQJLIN <nut01@yandex.ru>
2023-04-16 15:14:11 +0400

    KS_17_2

--- src/main/kotlin/Lesson17_Task2/Lesson17_2.kt
+++ src/main/kotlin/Lesson17_Task2/Lesson17_2.kt
@@ -1,7 +1,7 @@
 package Lesson17_Task2
 
 fun main() {
-val newShip = BattleShip("Неустрашимый",25,"Мурманск")
+    val newShip = BattleShip("Неустрашимый", 25, "Мурманск")
     newShip.printShipData()
     println("Введите новое имя корабля")
     newShip.name = readln()
@@ -9,19 +9,19 @@ val newShip = BattleShip("Неустрашимый",25,"Мурманск")
     newShip.printShipData()
 }
 
-class BattleShip(name:String, private val averageSpeed:Int, private val portHome:String )
-{
-   var name:String = name
-        set(value) {println("Имя корабля нельзя изменить")}
+class BattleShip(name: String, private val averageSpeed: Int, private val portHome: String) {
+    var name: String = name
+        set(value) {
+            println("Имя корабля нельзя изменить")
+        }
 
-
-    fun printShipData()
-    {
-        println("""
+    fun printShipData() {
+        println(
+            """
             ИМЯ: $name
             СРЕДНЯЯ СКРОСТЬ: $averageSpeed
             ПОРТ ПРИПИСКИ: $portHome
-        """.trimIndent())
+        """.trimIndent()
+        )
     }
-
 }
