73e5aef57c3523867b81349128500ec05adbba2e
AndrewQJLIN <nut01@yandex.ru>
2023-04-20 18:10:57 +0400

    KS_20_3 исправил без fun

--- src/main/kotlin/Lesson20_Task3/Lesson20_3.kt
+++ src/main/kotlin/Lesson20_Task3/Lesson20_3.kt
@@ -3,12 +3,12 @@ package Lesson20_Task3
 fun main() {
     val newPlayer = Player()
     newPlayer.tryOpenDoor()
-    newPlayer.hasKey=false
+    newPlayer.hasKey = false
     newPlayer.tryOpenDoor()
 }
 
 class Player() {
-   var hasKey:Boolean = true
+    var hasKey: Boolean = true
 
-    val tryOpenDoor = fun() = run { if (hasKey) println("Дверь открыта") else println("Нет ключа! дверь не открыть") }
+    val tryOpenDoor = { if (hasKey) println("Дверь открыта") else println("Нет ключа! дверь не открыть") }
 }
