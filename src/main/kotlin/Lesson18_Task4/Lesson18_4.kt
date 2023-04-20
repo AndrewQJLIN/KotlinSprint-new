300fd4af7c9e3ee91f3804f0ed093336f8dc783f
AndrewQJLIN <nut01@yandex.ru>
2023-04-20 17:44:35 +0400

    KS_18_4 исправлена опечатка, убрал лишний код, вып.форматирование

--- src/main/kotlin/Lesson18_Task4/Lesson18_4.kt
+++ src/main/kotlin/Lesson18_Task4/Lesson18_4.kt
@@ -1,12 +1,12 @@
 package Lesson18_Task4
 
 fun main() {
-    val listPackage:List<Package> = listOf(
+    val listPackage: List<Package> = listOf(
         RectanglePackage(0.10, 0.20, 0.15),
         RectanglePackage(0.18, 0.23, 0.11),
         CubePackage(0.54)
     )
-    listPackage.forEach { println(" ${it.returnSquare().toString()} м.кв") }
+    listPackage.forEach { println(" ${it.returnSquare()} м.кв") }
 }
 
 abstract class Package {
@@ -15,12 +15,12 @@ abstract class Package {
 
 class RectanglePackage(
     private val height: Double,
-    private val lenght: Double,
+    private val length: Double,
     private val width: Double
 ) : Package() {
-    override fun returnSquare(): Double = (height * lenght * 2) + (height * width * 2) + (lenght * width * 2)
+    override fun returnSquare(): Double = (height * length * 2) + (height * width * 2) + (length * width * 2)
 }
 
-class CubePackage( private val side:Double):Package() {
-    override fun returnSquare(): Double = side*side*6
+class CubePackage(private val side: Double) : Package() {
+    override fun returnSquare(): Double = side * side * 6
 }
